246ad82c1a4b84831d094ad304cf68c5e3679eb0
ppaulis <ppaulis@gmail.com>
2016-06-29 14:28:15 +0200

    finished Behat setup

--- features/articles.feature
+++ features/articles.feature
@@ -4,5 +4,15 @@ Feature: Articles
   I need to be able to manage the articles
 
   Scenario: Retrieve the unfiltered list of articles
+    Given an article with name "article1" and price "25"
+    And an article with name "article2" and price "35"
     When I send GET request to "/article"
-    Then echo last response
+    Then response should contain the article "article1" with price "25"
+    And response should contain the article "article2" with price "35"
+
+  Scenario: Retrieve the unfiltered list of articles
+    Given an article with name "article1" and price "25"
+    And an article with name "article2" and price "35"
+    When I send GET request to "/article"
+    Then response should contain the article "article1" with price "25"
+    And response should contain the article "article2" with price "35"
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
@@ -18,6 +19,27 @@ class FeatureContext implements Context, SnippetAcceptingContext
      * context constructor through behat.yml.
      */
     public function __construct()
+    {
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function purgeDatabase()
+    {
+    }
+
+    /**
+     * @Given an article with name :articleName and price :articlePrice
+     */
+    public function anArticleWithNameAndPrice($articleName, $articlePrice)
+    {
+    }
+
+    /**
+     * @Then response should contain the article :arg1 with price :arg2
+     */
+    public function responseShouldContainTheArticleWithPrice($arg1, $arg2)
     {
     }
 }
